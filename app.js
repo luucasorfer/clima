00cbf0c20cd8bdfd1319e1821e6f18ba9fd89f3a
luucasorfer <lucas.fernandes.94.lf@gmail.com>
2023-03-16 13:19:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ function buscarLocalizacao() {
   navigator.geolocation.getCurrentPosition(posicao => {
     const latitude = posicao.coords.latitude;
     const longitude = posicao.coords.longitude;
-    //console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
     buscarDados({ latitude, longitude });
   }, erro => {
     alert(`Não foi possível obter a localização. Erro: ${erro.message}`);
@@ -33,12 +32,9 @@ function buscarDados(coordenadas) {
     url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric`;
   }
 
-  //console.log(url);
-
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      //console.log(data);
       mostrarDados(data);
     })
     .catch(erro => console.error(`Erro ao obter os dados: ${erro}`));
